bb5f2b68621d2ef0abf0f7a4eaae92b510ba74ac
StanimalTheMan <sjc591@nyu.edu>
2021-07-13 19:38:25 -0400

    refactor state to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageLinkForm from "./components/ImageLinkForm";
+import DemographicData from "./components/DemographicData";
 import SignIn from "./components/SignIn";
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [box, setBox] = useState({});
+  const [ageProbability, setAgeProbability] = useState(null);
+  const [culturalAppearanceProbability, setCulturalAppearanceProbability] =
+    useState(null);
+  const [genderProbability, setGenderProbability] = useState(null);
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
+  const handleInputChange = (event) => {
+    console.log("here");
+    setInput(event.target.value);
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    setIsFormSubmitted(true);
+    setImageUrl(input);
+    const response = await fetch("http://localhost:5000/data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: input,
+      }),
+    });
+    console.log("hi", input);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    setCulturalAppearanceProbability(
+      data.results[0].outputs[2].data.regions[0].data.concepts
+    );
+    setAgeProbability(data.results[0].outputs[4].data.regions[0].data.concepts);
+    setGenderProbability(
+      data.results[0].outputs[3].data.regions[0].data.concepts
+    );
+  };
+
   return (
     <>
-      <ImageLinkForm />
+      <ImageLinkForm
+        handleFormSubmit={handleFormSubmit}
+        handleInputChange={handleInputChange}
+      />
+      {isFormSubmitted &&
+        ageProbability &&
+        culturalAppearanceProbability &&
+        genderProbability && (
+          <DemographicData
+            imgUrl={imageUrl}
+            ageData={ageProbability}
+            culturalAppearanceData={culturalAppearanceProbability}
+            genderData={genderProbability}
+          />
+        )}
       <SignIn />
     </>
   );
--- src/components/ImageLinkForm.js
+++ src/components/ImageLinkForm.js
@@ -1,64 +1,22 @@
-import React, { useState } from "react";
-import DemographicData from "./DemographicData";
+import React from "react";
 
-const ImageLinkForm = () => {
-  const [input, setInput] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
-  const [ageProbability, setAgeProbability] = useState(null);
-  const [culturalAppearanceProbability, setCulturalAppearanceProbability] =
-    useState(null);
-  const [genderProbability, setGenderProbability] = useState(null);
-  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  //   const imageUrlRef = useRef();
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    setIsFormSubmitted(true);
-    setImageUrl(input);
-    const response = await fetch("http://localhost:5000/data", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        url: input,
-      }),
-    });
-    console.log(response);
-    const data = await response.json();
-    console.log(data);
-    setCulturalAppearanceProbability(
-      data.results[0].outputs[2].data.regions[0].data.concepts
-    );
-    setAgeProbability(data.results[0].outputs[4].data.regions[0].data.concepts);
-    setGenderProbability(
-      data.results[0].outputs[3].data.regions[0].data.concepts
-    );
+const ImageLinkForm = ({ handleFormSubmit, handleInputChange }) => {
+  const onFormSubmit = (event) => {
+    handleFormSubmit(event);
   };
 
-  const handleInputChange = (event) => {
-    setInput(event.target.value);
+  const onInputChange = (event) => {
+    handleInputChange(event);
   };
 
   return (
-    <form onSubmit={handleFormSubmit}>
+    <form onSubmit={onFormSubmit}>
       <input
-        onChange={handleInputChange}
+        onChange={onInputChange}
         type="text"
         placeholder="Enter an image url:"
       ></input>
       <button type="submit">Fetch demographic data</button>
-      {isFormSubmitted &&
-        ageProbability &&
-        culturalAppearanceProbability &&
-        genderProbability && (
-          <DemographicData
-            imgUrl={imageUrl}
-            ageData={ageProbability}
-            culturalAppearanceData={culturalAppearanceProbability}
-            genderData={genderProbability}
-          />
-        )}
     </form>
   );
 };
