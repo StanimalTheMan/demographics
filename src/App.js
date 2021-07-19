d4ab0d2fb28416698cc4f0490c0cac9b6164974f
StanimalTheMan <sjc591@nyu.edu>
2021-07-19 14:04:51 -0400

    remove unnecessary console.log

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ const App = () => {
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
   const handleInputChange = (event) => {
-    console.log("here");
     setInput(event.target.value);
   };
 
